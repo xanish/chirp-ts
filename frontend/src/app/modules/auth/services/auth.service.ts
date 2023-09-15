ecc74fec4d6adbe9aa91c2a25ec32804f2e96639
xanish <danishali7866@gmail.com>
2023-09-15 19:34:37 +0530

    refactor auth service return types

--- frontend/src/app/modules/auth/services/auth.service.ts
+++ frontend/src/app/modules/auth/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/modules/core/services/api.service';
+import { TTokenResponse } from '../../shared/types/token-response.type';
+import { TUser } from '../../shared/types/user.type';
 import { ForgotPasswordOptions } from '../shared/types/forgot-password-options.type';
 import { LoginOptions } from '../shared/types/login-options.type';
 import { RegisterOptions } from '../shared/types/register-options.type';
@@ -10,23 +12,23 @@ import { ResetPasswordOptions } from '../shared/types/reset-password-options.typ
 export class AuthService {
   constructor(private apiService: ApiService) {}
 
-  forgotPassword(data: ForgotPasswordOptions): Observable<Object> {
+  forgotPassword(data: ForgotPasswordOptions): Observable<any> {
     return this.apiService.post('/auth/forgot-password', data);
   }
 
-  login(data: LoginOptions): Observable<Object> {
+  login(data: LoginOptions): Observable<TTokenResponse> {
     return this.apiService.post('/auth/login', data);
   }
 
-  register(data: RegisterOptions): Observable<Object> {
+  register(data: RegisterOptions): Observable<TUser> {
     return this.apiService.post('/auth/register', data);
   }
 
-  resetPassword(data: ResetPasswordOptions): Observable<Object> {
+  resetPassword(data: ResetPasswordOptions): Observable<TUser> {
     return this.apiService.post('/auth/reset-password', data);
   }
 
-  verify(token: string) {
+  verify(token: string): Observable<any> {
     return this.apiService.post(`/auth/verify/${token}`);
   }
 }
--- frontend/src/app/modules/shared/types/token-response.type.ts
+++ frontend/src/app/modules/shared/types/token-response.type.ts
@@ -1,3 +1,3 @@
-export type TokenResponse = {
+export type TTokenResponse = {
   token: string;
 };
