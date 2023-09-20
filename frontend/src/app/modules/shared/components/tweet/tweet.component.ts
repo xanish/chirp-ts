f67ba9db2c5b27e79d738e8486ef2a82eca9c5ab
xanish <danishali7866@gmail.com>
2023-09-20 11:21:37 +0530

    handle image grid logic

--- frontend/src/app/modules/shared/components/tweet/tweet.component.ts
+++ frontend/src/app/modules/shared/components/tweet/tweet.component.ts
@@ -10,4 +10,16 @@ import { Tweet } from '../../models/tweet.model';
 export class TweetComponent {
   @Input({ required: true }) tweet: Tweet = Tweet.default();
   attachmentType = AttachmentType;
+
+  gridClasses(attachmentCount: number): string {
+    switch (attachmentCount) {
+      case 4:
+      case 3:
+        return 'grid-cols-2 grid-rows-2';
+      case 2:
+        return 'grid-cols-2 grid-rows-1';
+      default:
+        return 'grid-cols-1 grid-rows-1';
+    }
+  }
 }
