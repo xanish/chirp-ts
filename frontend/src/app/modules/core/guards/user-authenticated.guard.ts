159da0dffec1eca7e2a12130bcbab8e02dd321fe
xanish <danishali7866@gmail.com>
2023-09-25 16:39:35 +0530

    not found to feed and redirect to login from user auth guard if unauthenticated

--- frontend/src/app/modules/core/guards/user-authenticated.guard.ts
+++ frontend/src/app/modules/core/guards/user-authenticated.guard.ts
@@ -2,18 +2,25 @@ import { Injectable, inject } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivateFn,
+  Router,
   RouterStateSnapshot,
 } from '@angular/router';
 import { TokenService } from '../services/token.service';
 
 @Injectable()
 export class UserAuthenticatedGuard {
-  constructor(private tokenService: TokenService) {}
+  constructor(private router: Router, private tokenService: TokenService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     // this is just a dummy call ideally need to call the server to
     // verify the authenticity of the token
-    return !!this.tokenService.token();
+    const authenticated = !!this.tokenService.token();
+
+    if (!authenticated) {
+      this.router.navigate(['auth/login']);
+    }
+
+    return authenticated;
   }
 }
 
