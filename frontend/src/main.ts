2dfcfa3635b5ab33875a0b44095382b173f88c58
xanish <danishali7866@gmail.com>
2023-10-09 19:59:48 +0530

    provide date pipe and fix edit profile height on 1366

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,30 +8,32 @@ import { InjectTokenInterceptor } from './app/modules/core/interceptors/inject-t
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
+import { DatePipe } from '@angular/common';
 
 
 bootstrapApplication(AppComponent, {
-	providers: [
-		importProvidersFrom(BrowserModule),
-		provideHttpClient(
+  providers: [
+    importProvidersFrom(BrowserModule),
+    provideHttpClient(
       withInterceptorsFromDi(),
     ),
-		{
-			provide: HTTP_INTERCEPTORS,
-			useClass: InjectTokenInterceptor,
-			multi: true,
-		},
-		{
-			provide: HTTP_INTERCEPTORS,
-			useClass: RefreshTokenInterceptor,
-			multi: true,
-		},
-		provideAnimations(),
-		provideToastr({
-			timeOut: 10000,
-			positionClass: 'toast-top-right',
-			preventDuplicates: true,
-		}),
-		provideRouter(AppRoutes)
-	]
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InjectTokenInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RefreshTokenInterceptor,
+      multi: true,
+    },
+    provideAnimations(),
+    provideToastr({
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
+    provideRouter(AppRoutes),
+    DatePipe,
+  ]
 }).catch(err => console.error(err));
